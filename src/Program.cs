52413ef9b17e44193ecbeed2439238929c413ba3
Turki Dhawi <T.Dhawi@outlook.com>
2024-03-18 15:06:10 +0300

    accepted input from user

--- src/Program.cs
+++ src/Program.cs
@@ -1,22 +1,34 @@
 ﻿class Temperature_Converter
 {
     public static void Main (String[] args) {
-        Console.WriteLine(TempConvert(32, "F"));
-        Console.WriteLine(TempConvert(100, "C"));
+
+        Console.WriteLine("Enter a temperature and its unit (C or F): ");
+        string input = Console.ReadLine() ?? "";
+
+        string[] parts = input.Split(" ");
+               
+        string temperatureUnit = parts[1].ToUpper();
+        double.TryParse(parts[0], out double temperatureValue);
+
+        // Input we want to converted 
+        Console.WriteLine(TempConvert(temperatureValue, temperatureUnit));
+
+
+        Console.ReadKey();
     }
 
-    static string TempConvert (int degree, string unit) {
+    static string TempConvert (double degree, string unit) {
         bool isTrue = String.Equals(unit, "F");
         double converted;
 
         if (isTrue)
         {
             converted = (degree - 32)*5 / 9;
-            return $"{converted} C";
+            return $"{converted:F2} C";
         } else
         {
             converted = (degree*9/5) + 32;
-            return $"{converted} F";
+            return $"{converted:F2} F";
         }
     }
 }
