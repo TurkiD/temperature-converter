aedd225e17bb1b1e31519d4b276ca329e3c994b3
Turki Dhawi <T.Dhawi@outlook.com>
2024-03-18 15:43:20 +0300

    app keeps running until user entered the magic word

--- src/Program.cs
+++ src/Program.cs
@@ -3,29 +3,39 @@
     public static void Main (String[] args) {
         try
         {
-            Console.WriteLine("Enter a temperature and its unit (C or F) or type Quit to exist the app:");
-            string input = Console.ReadLine() ?? "";
-
-            string[] parts = input.Split(" ");
-            if (parts.Length != 2)
+            while (true)
             {
-                Console.WriteLine("Invalid Input. Please Enter a temperature value and it's unit (C or F)");
+                Console.WriteLine("Enter a temperature and its unit (C or F) or type Quit to exist the app:");
+                string input = Console.ReadLine() ?? "";
+
+                // end the app
+                if (input.ToLower() == "quit")
+                {
+                    Console.WriteLine("Program is terminated");
+                    break;
+                }
+
+                // check if user entered more than 2 input or less
+                string[] parts = input.Split(" ");
+                if (parts.Length != 2)
+                {
+                    Console.WriteLine("Invalid Input. Please Enter a temperature value and it's unit (C or F)");
+                    continue;
+                }
+
+                // check if user entered numeric values
+                if (!double.TryParse(parts[0], out double temperatureValue))
+                {
+                    Console.WriteLine("Invalid Value. Please Enter a numeric value for temperature");
+                    continue;
+                } 
+
+                // convert unit to string 
+                string temperatureUnit = parts[1].ToUpper();
+
+                // Input we want to converted 
+                Console.WriteLine(TempConvert(temperatureValue, temperatureUnit));
             }
-
-            string temperatureUnit = parts[1].ToUpper();
-            if (temperatureUnit != "C" && temperatureUnit != "F")
-            {
-                Console.WriteLine("Invalid Unit. Please Enter a temperature value and it's unit (C or F)");
-            }
-
-            if (!double.TryParse(parts[0], out double temperatureValue))
-            {
-                Console.WriteLine("Invalid Value. Please Enter a numeric value for temperature");
-            } 
-
-            // Input we want to converted 
-            Console.WriteLine(TempConvert(temperatureValue, temperatureUnit));
-
         }
         catch (Exception e)
         {
@@ -38,14 +48,14 @@
         double converted;
         switch (unit)
         {
-            case "C": 
+            case "C":
+                converted = (degree*9/5) + 32;
+                return $"{converted:F2} F"; 
+            case "F": 
                 converted = (degree - 32)*5 / 9;
                 return $"{converted:F2} C";
-            case "F": 
-                converted = (degree*9/5) + 32;
-                return $"{converted:F2} F";
             default:
-                return "Something went wrong please try again";
+                return "Invalid Unit. Please Enter a temperature value and it's unit (C or F)";
         }
     }
 }
