fa99691b4b52b59b3d46798d2be2f5836ce34c76
Turki Dhawi <T.Dhawi@outlook.com>
2024-03-18 15:23:37 +0300

    added validation and changed the TempConvert from using if statment to switch case

--- src/Program.cs
+++ src/Program.cs
@@ -1,34 +1,51 @@
 ﻿class Temperature_Converter
 {
     public static void Main (String[] args) {
+        try
+        {
+            Console.WriteLine("Enter a temperature and its unit (C or F) or type Quit to exist the app:");
+            string input = Console.ReadLine() ?? "";
 
-        Console.WriteLine("Enter a temperature and its unit (C or F): ");
-        string input = Console.ReadLine() ?? "";
+            string[] parts = input.Split(" ");
+            if (parts.Length != 2)
+            {
+                Console.WriteLine("Invalid Input. Please Enter a temperature value and it's unit (C or F)");
+            }
 
-        string[] parts = input.Split(" ");
-               
-        string temperatureUnit = parts[1].ToUpper();
-        double.TryParse(parts[0], out double temperatureValue);
+            string temperatureUnit = parts[1].ToUpper();
+            if (temperatureUnit != "C" && temperatureUnit != "F")
+            {
+                Console.WriteLine("Invalid Unit. Please Enter a temperature value and it's unit (C or F)");
+            }
 
-        // Input we want to converted 
-        Console.WriteLine(TempConvert(temperatureValue, temperatureUnit));
+            if (!double.TryParse(parts[0], out double temperatureValue))
+            {
+                Console.WriteLine("Invalid Value. Please Enter a numeric value for temperature");
+            } 
 
+            // Input we want to converted 
+            Console.WriteLine(TempConvert(temperatureValue, temperatureUnit));
 
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
         Console.ReadKey();
     }
 
     static string TempConvert (double degree, string unit) {
-        bool isTrue = String.Equals(unit, "F");
         double converted;
-
-        if (isTrue)
+        switch (unit)
         {
-            converted = (degree - 32)*5 / 9;
-            return $"{converted:F2} C";
-        } else
-        {
-            converted = (degree*9/5) + 32;
-            return $"{converted:F2} F";
+            case "C": 
+                converted = (degree - 32)*5 / 9;
+                return $"{converted:F2} C";
+            case "F": 
+                converted = (degree*9/5) + 32;
+                return $"{converted:F2} F";
+            default:
+                return "Something went wrong please try again";
         }
     }
 }
