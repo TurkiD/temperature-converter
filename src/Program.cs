bdcd9c44fbb5b0ab293713b32ee58eaf1d3c3342
Turki Dhawi <T.Dhawi@outlook.com>
2024-03-18 14:20:33 +0300

    added tempConvert method

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Temperature_Converter
+{
+    public static void Main (String[] args) {
+        Console.WriteLine(TempConvert(32, "F"));
+        Console.WriteLine(TempConvert(100, "C"));
+    }
+
+    static string TempConvert (int degree, string unit) {
+        bool isTrue = String.Equals(unit, "F");
+        double converted;
+
+        if (isTrue)
+        {
+            converted = (degree - 32)*5 / 9;
+            return $"{converted} C";
+        } else
+        {
+            converted = (degree*9/5) + 32;
+            return $"{converted} F";
+        }
+    }
+}
